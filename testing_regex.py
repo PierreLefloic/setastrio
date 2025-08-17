eda91294bed4d7fdc575c77ae1ea2d638bf51f44
Pierre Lefloic <pierre.lefloic@usherbrooke.ca>
2025-08-17 12:42:54 -0400

    fixing gallery generation

---
+++ testing_regex.py
@@ -0,0 +1,31 @@
+import re
+import os
+
+# Folders
+full_folder = "images/full"
+thumb_folder = "images/thumbs"
+index_file = "to_be_matched.html"
+
+# Build gallery HTML entries (use forward slashes for web paths)
+entries = [
+    f'<a href="{full_folder}/{filename}" target="_blank">'
+    f'<img src="{thumb_folder}/{filename}" alt="{filename}" /></a>'
+    for filename in sorted(os.listdir(full_folder))
+    if filename.lower().endswith((".jpg", ".jpeg", ".png", ".webp"))
+]
+
+# Update index.html
+with open(index_file, "r", encoding="utf-8") as f:
+    html = f.read()
+
+new_html = re.sub(
+    r'(<div class="gallery-track"\s*>).*?(</div>)',
+    r'\1\n    ' + "\n    ".join(entries) + r'\2',
+    html,
+    flags=re.DOTALL | re.IGNORECASE
+)
+
+with open(index_file, "w", encoding="utf-8") as f:
+    f.write(new_html)
+
+print("Gallery updated in index.html!")
